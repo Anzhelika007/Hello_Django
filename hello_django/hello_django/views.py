51da8b981511862450caf400613ca2c56791d59d
Anzhalika Turlak <293852647@mail.ru>
2022-09-16 20:36:58 +0300

    lesson 1 done

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -7,8 +7,11 @@ def about(request):
     return render(request, 'about.html', {'gretting': a})
 
 
-def home(request):
-    return render(request, 'home.html')
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request, 'reverse.html', {'word': reverse})
 
-# def home(request):
-#     return HttpResponse('This is my home')
+
+def home(request):
+    return HttpResponse('This is my home')
